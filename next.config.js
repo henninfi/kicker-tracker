1dbe1c0fab570c62bc0affd6f99535ded0c42aab
henninfi <95130159+henninfi@users.noreply.github.com>
2023-10-27 09:25:04 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@
 module.exports = {
   reactStrictMode: true,
   experimental: { appDir: true },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "https://table-tennis-three.vercel.app/",
-        permanent: true,
-      },
-    ];
-  },
 };
